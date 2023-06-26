04b8d97dd63e4663a407aef41685c293b52b581d
vladnov138 <vladnov138@gmail.com>
2023-06-27 00:51:37 +0800

    Add responses for folders functions results

--- modules/responses.py
+++ modules/responses.py
@@ -32,6 +32,21 @@ def generate_bad_authdata_response():
     }}
 
 
+def generate_folder_exist_error():
+    """Returns data for cases when folder exists"""
+    return {'success': 'false', 'error': {'code': 420, 'description': 'The folder already exists'}}
+
+
+def generate_folder_not_found_error():
+    """Returns data for cases when folder not found"""
+    return {'success': 'false', 'error': {'code': 421, 'description': 'The folder not found'}}
+
+
+def generate_success_directories(directories: list):
+    """Generates response for reading directories"""
+    return {'status': 'success', 'error': None, 'directories': directories}
+
+
 def generate_success_regdata(name: str, token: str) -> dict:
     """Generates response for the successful registration"""
     return {'status': 'success', 'error': None, 'user_name': name, 'token': token}
