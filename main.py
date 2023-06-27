5145acf6566a9a957819e485da0be534a55ac739
vladnov138 <vladnov138@gmail.com>
2023-06-27 13:12:51 +0800

    Add logs

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import uvicorn
 from datetime import date, datetime
 
 from fastapi import FastAPI, UploadFile, File
+from loguru import logger
 from pydantic import EmailStr
 from sqlalchemy import create_engine
 
@@ -25,18 +26,23 @@ connection_data = connect()
 engine = connection_data[0]
 session = connection_data[1]
 
+
 @app.post("/sign_up")
 def sign_up(user_name: str, user_email: EmailStr, password: str) -> dict:
     token = generate_token()
+    logger.info("[Sign up] Received request to sign up a user")
     if check_user(engine, session, user_name):
+        logger.error(f"[Sign up] User {user_name} is already signed up")
         return generate_username_inuse_response()
     add_user(engine, session, user_name, user_email, password, token)
     storage.init_user_storage(user_name)
+    logger.info(f"[Sign up] User {user_name} signed up successfully")
     return generate_success_regdata(user_name, token)
 
 
 @app.post('/sign_in')
 def sign_in(user_email: EmailStr, password: str) -> dict:
+    logger.info("[Sign in] Received request to sign in a user")
     if authorization(engine, session, user_email, password) == 0:
         return generate_bad_authdata_response()
     token = search_token_by_email(engine, session, user_email)
@@ -46,67 +52,90 @@ def sign_in(user_email: EmailStr, password: str) -> dict:
 @app.post('/create_new_folder')
 def create_new_folder(token: str, name: str) -> dict:
     user_name = search_name_by_token(engine, session, token)
+    logger.info(f"[Create new folder] Received request to create new folder for user: {user_name}")
     if user_name:
         try:
             storage.create_folder(engine, session, name, user_name)
         except FolderExistException:
+            logger.error(f"[Create new folder] The folder has been already created by: {user_name}")
             return generate_folder_exist_error()
+        logger.info(f"[Create new folder] New folder was created by: {user_name}")
         return generate_success_response()
+    logger.info(f"[Create new folder] Wrong token for user: {user_name}")
     return generate_bad_token_response()
 
 
 @app.post('/delete_folder')
 def delete_folder(token: str, folder_id: int) -> dict:
     user_name = search_name_by_token(engine, session, token)
+    logger.info(f"[Delete folder] Received request to delete folder with id: {folder_id} for user: {user_name}")
     if user_name:
         try:
             storage.delete_folder(engine, session, user_name, folder_id)
+            logger.info(f"[Delete folder] The folder with id: {folder_id} was successfully deleted by: {user_name}")
         except FolderNotFound:
+            logger.error(f"[Delete folder] The folder with id: {folder_id} wasn't found for: {user_name}")
             return generate_folder_not_found_error()
         return generate_success_response()
+    logger.info(f"[Delete folder] Wrong token for user: {user_name}")
     return generate_bad_token_response()
 
 
 @app.post('/get_folders')
 def get_folders(token: str, limit: int) -> dict:
     user_name = search_name_by_token(engine, session, token)
+    logger.info(f"[Get folders] Received request to get folders for user: {user_name}")
     if user_name:
         user_id = get_user_id(engine, session, user_name)
         directories = get_directories(engine, session, user_id)
+        logger.info(f"[Get folders] Folders were given for user: {user_name}")
         return generate_success_directories(directories)
+    logger.error(f"[Get folders] Wrong token for user: {user_name}")
     return generate_bad_token_response()
 
 
 @app.post('/rename_folder')
 def rename_folder(token: str, folder_id: int, new_name: str) -> dict:
     user_name = search_name_by_token(engine, session, token)
+    logger.info(f"[Rename folder] Received request to rename folder with id: {folder_id} to {new_name} "
+                f"for user: {user_name}")
     if user_name:
         try:
             storage.update_folder_name(engine, session, user_name, folder_id, new_name)
-        except FolderExistException:
+        except FolderNotFound:
+            logger.error(f"[Rename folder] Folder with id: {folder_id} not found for user: {user_name}")
             return generate_folder_not_found_error()
+        logger.info(f"[Rename folder] Folder with id: {folder_id} was renamed to {new_name}: {user_name}")
         return generate_success_response()
+    logger.error(f"[Rename folder] Wrong token for user: {user_name}")
     return generate_bad_token_response()
 
 
 @app.post("/upload_data")
 async def upload_data(token: str, folder_id: int, file: UploadFile = File(...)) -> dict:
     user_name = search_name_by_token(engine, session, token)
+    logger.info(f"[Upload data] Received request to upload data to folder with id: {folder_id} "
+                f"for user: {user_name}")
     if user_name:
         try:
             await storage.create_file(engine, session, user_name, folder_id, file)
         except FolderNotFound:
+            logger.error(f"[Upload data] Folder with id: {folder_id} not found for user: {user_name}")
             return generate_folder_not_found_error()
+        logger.info(f"[Upload data] {file.filename} was uploaded to folder with id: {folder_id} "
+                    f"by user: {user_name}")
         return generate_success_response()
+    logger.error(f"[Upload data] Wrong token for user: {user_name}")
     return generate_bad_token_response()
 
 
-@app.get("/get_last_data")
-async def get_last_data(token: str, limit: int = 5) -> dict:
-    user_name = search_email_by_token(engine, session, token)
+@app.get("/get_data")
+async def get_data(token: str, limit: int = 5) -> dict:
+    user_name = search_name_by_token(engine, session, token)
     if user_name:
         user_id = get_user_id(engine, session, user_name)
-        return generate_success_wdata(get_files(user_id, limit=limit))
+        return generate_success_wdata(get_files(engine, session, user_id, limit=limit))
+    logger.error(f"[Upload data] Wrong token for user: {user_name}")
     return generate_bad_token_response()
 
 
@@ -115,12 +144,13 @@ async def get_data_by_date(token: str, start_date: date, finish_date: date) -> d
     user_name = search_name_by_token(engine, session, token)
     if user_name:
         user_id = get_user_id(engine, session, user_name)
-        return generate_success_wdata(get_dates(user_id, start_date, finish_date))
+        return generate_success_wdata(get_dates(engine, session, user_id, start_date, finish_date))
+    logger.error(f"[Upload data] Wrong token for user: {user_name}")
     return generate_bad_token_response()
 
 
 @app.post("/update_data")
-async def update_data(token: str, data_id: int, file: UploadFile = File(...)) -> dict:
+async def update_data(token: str, data_id: int, directory_id: int, file_id: int, file: UploadFile = File(...)) -> dict:
     user_name = search_email_by_token(engine, session, token)
     if user_name:
         filename = file.filename
@@ -130,6 +160,7 @@ async def update_data(token: str, data_id: int, file: UploadFile = File(...)) ->
         user_id = get_user_id(engine, session, user_name)
         update_file(engine, session, data_id, filename)
         return generate_success_response()
+    logger.error(f"[Upload data] Wrong token for user: {user_name}")
     return generate_bad_token_response()
 
 
@@ -137,9 +168,9 @@ async def update_data(token: str, data_id: int, file: UploadFile = File(...)) ->
 async def delete_data(token: str, data_id: int) -> dict:
     user_name = search_email_by_token(engine, session, token)
     if user_name:
-        user_id = get_user_id(engine, session, user_name)
         del_file(engine, session, data_id)
         return generate_success_response()
+    logger.error(f"[Upload data] Wrong token for user: {user_name}")
     return generate_bad_token_response()
 
 
--- modules/new_db_logic.py
+++ modules/new_db_logic.py
@@ -1,3 +1,5 @@
+from loguru import logger
+
 from modules.connect_db import User, Uploaded_file, Directory
 import secrets
 from datetime import datetime
@@ -10,6 +12,7 @@ def add_user(engine, session, nickname:str, email:str, password:str, token=secre
         db.add(new_user)
         db.commit()
         new_user_id = new_user.id
+        logger.info(f"[CRUD user] New user: {nickname} was created")
     return new_user_id
 
 
@@ -19,103 +22,124 @@ def add_directory(engine, session, user_id:int, name_directory:str):
         db.add(new_directory)
         db.commit()        
         new_directory_id = new_directory.id
+        logger.info(f"[CRUD directory] New directory for user with id: {user_id} was created")
     return new_directory_id
 
 
-def add_file(engine, session, user_id:int, directory_id:int, file:str, range_start:datetime, range_end:datetime, date=datetime.now()):
+def add_file(engine, session, user_id: int, directory_id: int, file: str,
+             range_start: datetime, range_end: datetime, date=datetime.now()):
     with session(autoflush=False, bind=engine) as db:
-        new_file = Uploaded_file(user_id = user_id, date=date,
-                        directory_id = directory_id, file = file,
-                        range_start = range_start, range_end = range_end)
+        new_file = Uploaded_file(user_id=user_id, date=date,
+                        directory_id=directory_id, file=file,
+                        range_start=range_start, range_end=range_end)
         db.add(new_file)
         db.commit()     
         new_file_id = new_file.id
+        logger.info(f"[CRUD file] Add new file: {file} to directory with id: {directory_id} by "
+                    f"user with id {user_id}")
     return new_file_id
 
 
 def check_user(engine, session, nickname:str):
     with session(autoflush=False, bind=engine) as db:
         user = db.query(User).filter(User.nickname == nickname).first()
+    if user:
+        logger.info(f"[CRUD user] User: {nickname} was found")
+    else:
+        logger.error(f"[CRUD user] User: {nickname} wasn't found")
     return bool(user)
 
 
-def authorization(engine, session, email:str, password:str):
+def authorization(engine, session, email: str, password: str):
     with session(autoflush=False, bind=engine) as db:
-        user = db.query(User).filter(User.email==email).filter(User.password==password).first()
+        user = db.query(User).filter(User.email == email).filter(User.password == password).first()
+    if user:
+        logger.info(f"[CRUD user] {user.nickname} was successfully signed in")
+    else:
+        logger.error(f"[CRUD user] User wasn't signed in")
     return bool(user)
 
 
 def search_email_by_token(engine, session, token: str):
     with session(autoflush=False, bind=engine) as db:
         user = db.query(User).filter(User.token == token).first()
+        logger.info(f"[Search email] Get user's email for user: {user.nickname} by token")
     return user.email
 
 
 def search_name_by_token(engine, session, token: str):
     with session(autoflush=False, bind=engine) as db:
         user = db.query(User).filter(User.token == token).first()
+        logger.info(f"[Search name] Get user's name for user: {user.nickname} by token")
     return user.nickname
 
 
 def search_token_by_email(engine, session, email: str):
     with session(autoflush=False, bind=engine) as db:
         user = db.query(User).filter(User.email == email).first()
+        logger.info(f"[Search token] Get user's token for user: {user.nickname} by email")
     return user.token
 
 
 def get_user_id(engine, session, nickname: str):
     with session(autoflush=False, bind=engine) as db:
         user = db.query(User).filter(User.nickname == nickname).first()
+        logger.info(f"[CRUD user] Get user id by nickname: {nickname}")
     return user.id
 
 
-def get_files(engine, session, user_id:int, sort_max=0, limit=10):
+def get_files(engine, session, user_id: int, sort_max=0, limit=10):
     if sort_max:
         with session(autoflush=False, bind=engine) as db:
-            files = db.query(Uploaded_file).filter(Uploaded_file.user_id==user_id).order_by(Uploaded_file.date.asc()).limit(limit).all()
-    
+            files = db.query(Uploaded_file).filter(Uploaded_file.user_id == user_id).order_by(Uploaded_file.date.asc()).limit(limit).all()
     else:
         with session(autoflush=False, bind=engine) as db:
-            files = db.query(Uploaded_file).filter(Uploaded_file.user_id==user_id).order_by(Uploaded_file.date.desc()).limit(limit).all()
+            files = db.query(Uploaded_file).filter(Uploaded_file.user_id == user_id).order_by(Uploaded_file.date.desc()).limit(limit).all()
     return files
 
 
 def get_directories(engine, session, user_id: int):
     with session(autoflush=False, bind=engine) as db:
         directories = db.query(Directory).filter(Directory.user_id == user_id).all()
+        logger.info(f"[CRUD directory] User with id: {user_id} get directories")
     return directories
 
 
 def get_directory_id_by_name(engine, session, user_id: int, name_directory: str):
     with session(autoflush=False, bind=engine) as db:
-        directory = db.query(Directory).filter(Directory.user_id==user_id, Directory.name_directory==name_directory).first()
+        directory = db.query(Directory).filter(Directory.user_id == user_id, Directory.name_directory == name_directory).first()
+        logger.info(f"[CRUD directory] User with id: {user_id} get directory id by name {name_directory}")
     return directory.id
 
 
-def get_directory_by_id(engine, session, directory_id:int): #get directory
+def get_directory_by_id(engine, session, directory_id: int):
     with session(autoflush=False, bind=engine) as db:
-        directory = db.query(Directory).filter(Directory.id==directory_id).first()
+        directory = db.query(Directory).filter(Directory.id == directory_id).first()
+        logger.info(f"[CRUD directory] Get directory id by id {directory_id}")
     return directory
 
 
-def get_dates(engine, session, user_id:int, first_date, second_date, limit=10):
+def get_dates(engine, session, user_id: int, first_date, second_date, limit=10):
     with session(autoflush=False, bind=engine) as db:
-        files = db.query(Uploaded_file).filter(Uploaded_file.range_start>=first_date, Uploaded_file.range_end<=second_date).all()
+        files = db.query(Uploaded_file).filter(Uploaded_file.range_start >= first_date,
+                                               Uploaded_file.range_end <= second_date).all()
     return files
 
 
-def del_user(engine, session, nickname:str):
+def del_user(engine, session, nickname: str):
     with session(autoflush=False, bind=engine) as db:
-        user = db.query(User).filter(User.nickname==nickname).first()
+        user = db.query(User).filter(User.nickname == nickname).first()
         db.delete(user)
         db.commit()
+        logger.info(f"[CRUD user] Delete user: {nickname}")
 
 
-def del_file(engine, session, file_id:int):
+def del_file(engine, session, file_id: int):
     with session(autoflush=False, bind=engine) as db:
-        file = db.query(Uploaded_file).filter(Uploaded_file.id==file_id).first()
+        file = db.query(Uploaded_file).filter(Uploaded_file.id == file_id).first()
         db.delete(file)
         db.commit()
+        logger.info(f"[CRUD file] Delete file with id: {file_id}")
 
 
 def del_directory(engine, session, directory_id: int):
@@ -123,6 +147,7 @@ def del_directory(engine, session, directory_id: int):
         directory = db.query(Directory).filter(Directory.id == directory_id).first()
         db.delete(directory)
         db.commit()
+        logger.info(f"[CRUD directory] Delete directory by id {directory_id}")
 
 
 def update_file(engine, session, file_id: int, new_name: str, date=datetime.now()):
@@ -133,8 +158,11 @@ def update_file(engine, session, file_id: int, new_name: str, date=datetime.now(
         db.commit()
 
 
+
 def update_name_directory(engine, session, directory_id: int, new_name: str):
     with session(autoflush=False, bind=engine) as db:
         directory = db.query(Directory).filter(Directory.id == directory_id).first()
+        old_name = directory.name_directory
         directory.name_directory = new_name
         db.commit()
+        logger.info(f"[CRUD directory] Rename directory with id: {directory_id} from {old_name} to {new_name}")
