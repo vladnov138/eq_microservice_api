98d9c3e9e31b512911248c0d054705d308310810
vladnov138 <vladnov138@gmail.com>
2023-07-03 13:49:34 +0800

    Refactor code

--- app/database.py
+++ app/database.py
@@ -10,8 +10,12 @@ def create_bd(engine):  # create all tables
     Base.metadata.create_all(bind=engine)
 
 
-def connect(name_db='main.db', way='/app'):
-
+def connect(name_db='main.db', way=''):
+    # sqlite_database = f"sqlite://{way}/database/{name_db}"
+    if 'app' not in os.getcwd():
+        way += '/app'
+    if 'test' in os.getcwd() and way == '':
+        way = '/..'
     sqlite_database = f"sqlite://{way}/database/{name_db}"
     engine = create_engine(sqlite_database)
     session = sessionmaker(autoflush=False, bind=engine)
